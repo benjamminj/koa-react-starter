e5d3d3bb7359b255b0e18ce32abe33dbefe9abdc
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-04-06 12:50:05 -0700

    setup: Start a basic server

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,11 @@
+import { createServer } from 'http'
 import Koa from 'koa'
+
+const app = new Koa()
+
+const server = createServer(app)
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  console.log(`Listening for connections on ${server.address().port}`);
+})
