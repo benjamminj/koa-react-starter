f5c691454d7f6242f3d592099aba2d95d2201eb4
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-04-06 14:31:22 -0700

    setup: organize client-side js

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
-export default () => (<div />)
+import App from './components'
 
-console.log('foo')
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
