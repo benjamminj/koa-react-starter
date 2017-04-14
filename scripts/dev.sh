d7440dd9503e717109f1f4e359d56a73f974329f
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-04-14 12:20:31 -0700

    deployment: change `build` to `build:client` for dev

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,4 @@
 # builds the client-side code first
 # starts the server in development mode, using babel to compile on the fly.
-npm run build
+npm run build:client
 node -r babel-register -r babel-polyfill src/server
